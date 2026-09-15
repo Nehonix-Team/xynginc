9ef8a701e0a730b52b954cc3a071e06cb508cc89
iDevo <idevo.eleazar@gmail.com>
2026-09-15 17:20:28 +0000

    feat(ui): add automatic system theme adaptation (prefers-color-scheme light/dark) to all pages

--- core-go/main.go
+++ core-go/main.go
@@ -15,7 +15,7 @@ import (
 
 var rootCmd = &cobra.Command{
 	Use:     "xynginc",
-	Version: "go-ed-1.1.18",
+	Version: "go-ed-1.1.19",
 	Short:   "XyPriss Nginx Controller - Simplified Nginx and SSL management",
 	PersistentPreRun: func(cmd *cobra.Command, args []string) {
 		// Read-only commands do not require root
